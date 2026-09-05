2a76bb1f70ec42cc6465555e66512301f1730645
Neel Parmar <yg159120@gmail.com>
2026-09-06 03:02:20 +0530

    commit

--- people-pay360/people-pay360/.next/dev/static/chunks/_1dh94un._.js
+++
@@ -1,5 +0,0 @@
-(globalThis["TURBOPACK_CHUNK_LISTS"] || (globalThis["TURBOPACK_CHUNK_LISTS"] = [])).push({
-    script: typeof document === "object" ? document.currentScript : undefined,
-    chunks: ["static/chunks/[root-of-the-server]__1iww7dt._.css","static/chunks/_0zrzuvi._.js","static/chunks/node_modules_0p3-idf._.js","static/chunks/_1sgmwfl._.js","static/chunks/node_modules_lucide-react_dist_esm_icons_0zb0ol5._.js"],
-    source: "entry"
-});
